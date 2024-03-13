261c32bad0bf03716ef6e9d50321856415219f18
Cyro <coolronitchs@gmail.com>
2024-03-14 02:39:54 +0530

    order fetched using api

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
 import CreateOrder from "./features/order/CreateOrder.jsx";
-import Order from "./features/order/Order.jsx";
+import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 
 const router = createBrowserRouter([
@@ -23,7 +23,12 @@ const router = createBrowserRouter([
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <div>
       <Link to="/">Home</Link>
+      <SearchOrder />
       <p>Header Component</p>
     </div>
   );
