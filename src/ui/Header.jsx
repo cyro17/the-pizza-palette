e897a8965884d79fb8837a837212ab0b1f4d25e5
Cyro <coolronitchs@gmail.com>
2024-03-15 01:56:46 +0530

    tailwind setup

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,8 +3,10 @@ import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
-    <div>
-      <Link to="/">Home</Link>
+    <div className="bg-yellow-400">
+      <Link to="/">
+        <strong>The Pizza Palettle</strong>
+      </Link>
       <SearchOrder />
       <p>Header Component</p>
     </div>
